da577fbc0371f1a4fc7b821fbcd93a2ad1e5a62f
Xavier Garcia Buils <xgbuils@gmail.com>
2017-08-13 11:27:36 +0200

    add slice method as a dependency

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
-class SliceIterable {
-    constructor (iterable) {
-        this.iterable = iterable
-        this.start = 0
-        this.end = Infinity
-    }
+const slice = require('slice-iterable-method')
 
-    slice (start, end) {
-        start = Math.max(start, 0)
-        const newEnd = Math.min(this.start + end, this.end)
-        if (start === 0 && newEnd === this.end) {
-            return this
-        }
-        const obj = Object.create(SliceIterable.prototype)
-        obj.start = this.start + start
-        obj.end = newEnd
-        obj.iterable = this.iterable
-        return obj
-    }
+function SliceIterable (iterable) {
+    this.iterable = iterable
+    this.start = 0
+    this.end = Infinity
+}
 
-    * [Symbol.iterator] () {
-        const iterator = this.iterable[Symbol.iterator]()
-        const end = this.end
-        const start = this.start
-        for (let i = 0; i < end; ++i) {
-            const {value, done} = iterator.next()
-            if (done) {
-                return
-            } else if (i >= start) {
-                yield value
+Object.defineProperties(SliceIterable.prototype, {
+    slice: {
+        value: slice
+    },
+    [Symbol.iterator]: {
+        * value () {
+            const iterator = this.iterable[Symbol.iterator]()
+            const end = this.end
+            const start = this.start
+            for (let i = 0; i < end; ++i) {
+                const {value, done} = iterator.next()
+                if (done) {
+                    return
+                } else if (i >= start) {
+                    yield value
+                }
             }
         }
     }
-}
+})
 
 module.exports = SliceIterable
